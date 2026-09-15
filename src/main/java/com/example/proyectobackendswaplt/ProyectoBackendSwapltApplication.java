ff6e6f5bcb62dad304edbd232a61a477598ff8ff
walteraquino <walter.aquino@utec.edu.pe>
2026-09-15 09:16:51 -0500

    First commit

---
+++ src/main/java/com/example/proyectobackendswaplt/ProyectoBackendSwapltApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectobackendswaplt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBackendSwapltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoBackendSwapltApplication.class, args);
+    }
+
+}
